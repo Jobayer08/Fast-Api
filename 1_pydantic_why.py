730887922a624d8d3d7d79888e81bcffe87d0122
Jobayer <mdabduljobayer570@gmail.com>
2025-10-11 04:13:31 +0600

    Update 1_pydantic_why.py

--- 1_pydantic_why.py
+++ 1_pydantic_why.py
@@ -1,10 +1,17 @@
-from pydantic import BaseModel
+from pydantic import BaseModel,EmailStr,Field
+from typing import List,Dict,Optional
 class Patient(BaseModel):
     name: str
-    age: int
+    email:EmailStr
+    age: int=Field(gt=0,lt=150)
+    weight: float
+    married: Optional[bool] = None
+    alergies: List[str]
+    contacts: Dict[str, str]
 def insert_patient_data(p: Patient):
-    print(f"Patient {p.name} of age {p.age} inserted into the database.")
+    print(f"Patient {p.name} of age {p.age} inserted into the database.{p.married}")
 
-patient_info={"name":"John Doe","age":30}
+patient_info={"email":'abc@gmail.com',"name":"John Doe","age":300,"weight":70.5,
+              "alergies":["pollen","nuts"],"contacts":{"emergency":"123-456-7890","doctor":"987-654-3210"}}
 patient1=Patient(**patient_info)
 insert_patient_data(patient1)
