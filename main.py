420486bbe41e62d0cf728c17ec2e212ff9ac1d80
Abdul Jobayer <mdabduljobayer570@gmail.com>
2026-04-27 23:43:34 +0600

    quary parameter

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from fastapi import FastAPI
 
 app = FastAPI()
-@app.get("/")
-def index():
-    return "Hello, World!"
+@app.get("/blog")
+def index(limi=10, published: bool = True, sort: str = None):
+    if published:
+     return f"{limi} publish"
+    else:
+        return f"{limi} not publish"
 
 @app.get("/blog/{id}")
 def show(id: int):
