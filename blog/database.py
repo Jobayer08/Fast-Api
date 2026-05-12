b0a6c31ae83e24d62aba938e76035b9752696f95
Abdul Jobayer <mdabduljobayer570@gmail.com>
2026-05-12 21:39:00 +0600

    database_connection

---
+++ blog/database.py
@@ -0,0 +1,11 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+SQLALCHEMY_DATABASE_UR = 'sqlite:///./blog.db'
+
+engine = create_engine(SQLALCHEMY_DATABASE_UR, connect_args={"check_same_thread": False})
+
+session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+bas = declarative_base()
--- blog/main.py
+++ blog/main.py
@@ -1,11 +1,9 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
+from . import schemas
 app = FastAPI()
 
-class Blog(BaseModel):
-    title: str
-    body: str
+
 
 @app.post('/blog')
-def create(req: Blog):
+def create(req: schemas.Blog):
     return req
