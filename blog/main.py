c4be2aa1a601e3e919c634f8cbb955e697e4dcd0
Abdul Jobayer <ug2102008@cse.pstu.ac.bd>
2026-07-26 23:59:16 +0600

    exception and status code

--- blog/main.py
+++ blog/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends,status,Response,HTTPException
 from .import schemas
 from .import models
 from .database import engine,session
@@ -15,7 +15,7 @@ def get_db():
     finally:
         db.close() 
 
-@app.post('/blog')
+@app.post('/blog',status_code=status.HTTP_201_CREATED)
 def create(req: schemas.Blog,db: Session=Depends(get_db)):
     new_blog=models.blog(title=req.title,body=req.body)
     db.add(new_blog)
@@ -29,7 +29,11 @@ def all(db: Session=Depends(get_db)):
     blogs=db.query(models.blog).all()
     return blogs
 
-@app.get('/blog/{id}')
-def show(id,db: Session=Depends(get_db)):
+@app.get('/blog/{id}',status_code=200)
+def show(id,response: Response,db: Session=Depends(get_db)):
     blog=db.query(models.blog).filter(models.blog.id==id).first()
+    if not blog:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f'Blog with the id {id} is not available')
+        # response.status_code = status.HTTP_404_NOT_FOUND
+        # return {'detail': f'Blog with the id {id} is not available'}
     return blog
