16d3efe52564c5f57e0ce02faefb9d4f730a29e7
Abdul Jobayer <ug2102008@cse.pstu.ac.bd>
2026-08-07 19:34:14 +0600

    delete

--- blog/main.py
+++ blog/main.py
@@ -23,6 +23,13 @@ def create(req: schemas.Blog,db: Session=Depends(get_db)):
     db.refresh(new_blog)
     return new_blog
 
+@app.delete('/blog/{id}',status_code=status.HTTP_204_NO_CONTENT)
+def destroy(id,db: Session=Depends(get_db)):
+    blog=db.query(models.blog).filter(models.blog.id==id).delete(synchronize_session=False)
+    db.commit()
+    return 'done'
+    
+
 
 @app.get('/blog')
 def all(db: Session=Depends(get_db)):
